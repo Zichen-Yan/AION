5b6d3519d0313ecb0c5c9b761f8218e2f4161911
Zichen-Yan <yanzichen0906@gmail.com>
2026-05-08 11:52:53 +0800

    update

--- DroneSim/scripts/install_px4.sh
+++ DroneSim/scripts/install_px4.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
 set -e
 
-MODULE_NAME="PX4-Autopilot"
-
-# Install yq if missing
-if ! command -v yq &>/dev/null; then
-    sudo apt update
-    sudo snap install yq
+cd ~
+if [ ! -d "PX4-Autopilot" ]; then
+    git clone https://github.com/PX4/PX4-Autopilot.git --recursive
 fi
-
-# Clone PX4
-URL=$(yq e ".github_repos[] | select(.name == \"$MODULE_NAME\") | .url" $CONFIG_FILE)
-BRANCH=$(yq e ".github_repos[] | select(.name == \"$MODULE_NAME\") | .branch" $CONFIG_FILE)
-MODULE_DIR="$HOME/PX4-Autopilot"
-
-if [ ! -d "$MODULE_DIR" ]; then
-    git clone -b "$BRANCH" "$URL" "$MODULE_DIR" --recursive
-else
-    echo "[INFO] $MODULE_DIR already exists, skipping clone."
-fi
-
-bash "$MODULE_DIR/Tools/setup/ubuntu.sh" --no-sim-tools
-make -C "$MODULE_DIR" DONT_RUN=1 px4_sitl_default none
-
-# Download QGroundControl
-QGC_NAME="QGroundControl.AppImage"
-QGC_DIR="$SETUP_DIR/submodules/QGroundControl"
-QGC_VERSION=$(yq e ".github_repos[] | select(.name == \"$MODULE_NAME\") | .QGC_VERSION" $CONFIG_FILE)
-QGC_URL="https://github.com/mavlink/qgroundcontrol/releases/download/$QGC_VERSION/$QGC_NAME"
-
-if [ ! -f "$QGC_DIR/$QGC_NAME" ]; then
-    wget "$QGC_URL" -P "$QGC_DIR"
-    chmod +x "$QGC_DIR/$QGC_NAME"
-else
-    echo "[INFO] $QGC_NAME already exists, skipping download."
-fi
+cd PX4-Autopilot
+git checkout v1.16.0
+git submodule update --init --recursive
+bash ./Tools/setup/ubuntu.sh
+make px4_sitl
