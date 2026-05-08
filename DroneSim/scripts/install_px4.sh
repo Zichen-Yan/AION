6bd92f2e1f93d98a108a4855f381608dae004e39
Zichen-Yan <yanzichen0906@gmail.com>
2026-05-08 17:35:02 +0800

    update

--- DroneSim/scripts/install_px4.sh
+++ DroneSim/scripts/install_px4.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -e
 
-#cd ~
-#if [ ! -d "PX4-Autopilot" ]; then
-#    git clone --branch v1.16.0 https://github.com/PX4/PX4-Autopilot.git --recursive
-#fi
-#cd PX4-Autopilot
-#git submodule update --init --recursive
-#bash ./Tools/setup/ubuntu.sh
-#make px4_sitl
-
 export MODULE_DIR=~/PX4-Autopilot
 
-git clone -b v1.13.0_raynor \
+git clone -b v1.16.0 \
   https://github.com/Temasek-Dynamics/PX4-Autopilot.git \
   $MODULE_DIR \
   --recursive
